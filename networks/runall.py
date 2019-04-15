d7e50b790579e28e3f85e40363319284ca4508ea
Chris Perlette <chrisperlette@gmail.com>
2019-04-14 19:50:02 -0600

    working on write up

--- networks/runall.py
+++ networks/runall.py
@@ -1,12 +1,3 @@
-#import Prairies10temp
-#import Prairies5temp
-#import Canada10temp
-#import Canada5temp
-#import Prairies10pre
-#import Prairies5pre
-#import Canada10pre
-#import Canada5pre
-
 from Full5 import fitter5
 from Full10 import fitter10
 
